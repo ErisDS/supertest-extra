d10b2a4fab8635984813aab088ac2668328a0785
Ryan Kelley <ryan.kelley@royaljay.net>
2015-09-22 12:11:27 -0600

    Initial commit and version

---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = function (superagent) {
+
+    /**
+     * Module dependencies
+     */
+
+    var agent = superagent.Request;
+
+    agent.prototype.sign = function (oauth, token) {
+        this.oauth = oauth;
+        this.token = token;
+        return this;
+    };
+
+    agent.prototype.signOauth2 = function () {
+        this.set(header, this.oauth.buildAuthHeader(this.token));
+    };
+
+    /**
+     * Overrides .request() to add the OAuth2 access token query param if needed.
+     * This cannot happen during .end() because the "query" params get processed
+     * before that here in the request() function.
+     */
+
+    var oldRequest = Request.prototype.request;
+
+    agent.prototype.request = function () {
+        this.request = oldRequest
+        if (this.oauth && this.oauth._request) {
+            this.signOauth2();
+        }
+
+        return this.request();
+    }
+
+};
